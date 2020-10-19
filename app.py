98109729b11aa3377e00bd3a921df0ef03360251
Amie Johnstone <amiejohnstone18@gmail.com>
2020-10-19 08:31:59 +0000

    Changed the index.html recipe cards to link from add_recipes on MongoDB

--- app.py
+++ app.py
@@ -20,28 +20,34 @@ mongo = PyMongo(app)
 @app.route("/")
 @app.route("/index")
 def index():
-    index = mongo.db.index.find()
-    return render_template("index.html", index=index)
+    add_recipes = mongo.db.add_recipes.find()
+    return render_template("index.html", add_recipes=add_recipes)
 
 
 @app.route("/add_recipes", methods=["GET", "POST"])
 def add_recipes():
     if request.method == "POST":
         recipe = {
-                "author": request.form.get("author"),
-                "recipe_name": request.form.get("recipe_name"),
-                "description": request.form.get("description"),
-                "preptime": request.form.get("preptime"),
-                "bakingtime": request.form.get("bakingtime"),
-                "serves": request.form.get("serves"),
-                "ingredients": request.form.get("ingredients"),
-                "method": request.form.get("method")
-            }
+            "author": request.form.get("author"),
+            "recipe_name": request.form.get("recipe_name"),
+            "description": request.form.get("description"),
+            "preptime": request.form.get("preptime"),
+            "bakingtime": request.form.get("bakingtime"),
+            "serves": request.form.get("serves"),
+            "ingredients": request.form.get("ingredients"),
+            "method": request.form.get("method")
+        }
 
         mongo.db.add_recipes.insert_one(recipe)
         flash("Recipe Uploaded")
         return redirect(url_for("add_recipes"))
     return render_template("add_recipes.html")
+
+
+@app.route("/view_more")
+def view_more():
+    view_more = mongo.db.view_more.find()
+    return render_template("view_more.html", view_more=view_more)
 
 
 if __name__ == "__main__":
